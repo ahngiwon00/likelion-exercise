98e4bf685aa09b5f8680739c9754838fcdcf1c2f
ahngiwon00 <ahngiwon00@naver.com>
2022-10-19 13:06:21 +0900

    관계 설정 기능 - (어떤 조합을 사용할 것인지?)

---
+++ src/main/java/com/dao/UserDaoFactory.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+public class UserDaoFactory {
+    public UserDao awsUserDao(){
+        UserDao userDao = new UserDao(new AwsConnectionMaker());
+        return userDao;
+    }
+    public UserDao localUserDao(){
+        UserDao userDao = new UserDao(new LocalConnectionMaker());
+        return userDao;
+    }
+}
--- src/test/java/com/UserDaoTest.java
+++ src/test/java/com/UserDaoTest.java
@@ -3,6 +3,7 @@ package com;
 import com.dao.AWSUserDaoImpl;
 import com.dao.AwsConnectionMaker;
 import com.dao.UserDao;
+import com.dao.UserDaoFactory;
 import com.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,8 @@ import java.sql.SQLException;
 class UserDaoTest {
     @Test
     void addAndSelect() throws Exception {
-        UserDao userDao = new UserDao(new AwsConnectionMaker());
+        //UserDao userDao = new UserDao(new AwsConnectionMaker());
+        UserDao userDao = new UserDaoFactory().awsUserDao();
         String id = "15";
         //userDao.add(new User(id,"raddar","1111"));
         //User user = userDao.get(id);
